0bbed55f363120257a4fe79863bf7c04a2a27acd
williamsb8 <william_libra_04@hotmail.com>
2022-09-26 16:40:51 -0500

    Implementación del ngClass en el componente defaultUser.

--- src/app/modules/user/default-user/default-user.component.ts
+++ src/app/modules/user/default-user/default-user.component.ts
@@ -14,21 +14,63 @@ export class DefaultUserComponent implements OnInit {
   public isShow = false;
   public msg = '';
 
+  // public classStyle = ['dev-card', 'dev-card-primary'];
+
+  public cards: {
+    title: string;
+    body?: string;
+    isPrimary: boolean;
+    isDisabled: boolean;
+  }[] = [
+    {
+      title: 'Tarjeta Principal',
+      isPrimary: true,
+      isDisabled: false
+    },
+    {
+      title: 'Tarjeta Secundaria',
+      isPrimary: false,
+      isDisabled: false
+    },
+    {
+      title: 'Tarjeta Desactivada',
+      isPrimary: true,
+      isDisabled: true
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  showError() {
+  showError(): void {
     this.style.background = 'red';
     this.msg = '* Hubo un error!';
     this.isShow = true;
 
   }
-  showSuccess() {
+  showSuccess(): void {
     this.style.background = 'green';
     this.msg = '* Todo Beleennn!';
     this.isShow = true;
   }
 
+  changeStatus(card: {
+    title: string,
+    body?: string,
+    isPrimary?: boolean,
+    isDisabled?: boolean;
+  }) {
+    card.isPrimary = !card.isPrimary;
+  }
+
+  disabledCard(card: {
+    title: string,
+    body?: string,
+    isPrimary?: boolean,
+    isDisabled?: boolean;
+  }) {
+    card.isDisabled = !card.isDisabled;
+  }
 }
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: UserListComponent
   },
   {
-    path: 'ngStyle',
+    path: 'directivas',
     component: DefaultUserComponent
   },
   {
